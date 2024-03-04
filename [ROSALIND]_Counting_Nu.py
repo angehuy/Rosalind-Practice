28a88c0984867252a891baee4ac3b13fdf3140be
angehuy <162070926+angehuy@users.noreply.github.com>
2024-03-03 20:27:46 -0500

    [ROSALIND]_Counting_Nu.py

--- [ROSALIND]_Counting_Nu.py
+++ [ROSALIND]_Counting_Nu.py
@@ -3,20 +3,24 @@
 
 # Problem: Given string of DNA, return 4 integars separated by space of the counts of each nucleotide
 
-string = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+def counting(string) :
+    # Initialize a dictionary to store counts for each symbol
+    symbol_counts = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
 
-# Initialize a dictionary to store counts for each symbol
-symbol_counts = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
-
-# Iterate through each letter in the string
-for letter in string:
+    # Iterate through each letter in the string
+    for letter in string:
     # Increment the corresponding counter in the dictionary
-    if letter in symbol_counts: # checks if key name exists in dict
-        symbol_counts[letter] += 1 # accesses the key and increments the value
+        if letter in symbol_counts: # checks if key name exists in dict
+            symbol_counts[letter] += 1 # accesses the key and increments the value
 
-string = ""
+    string = ""
 
-for symbol, count in symbol_counts.items():
-    # .items() returns each key:value pair as tuple
-    string += str(count) + " "
-print(string)
+    for symbol, count in symbol_counts.items():
+        # .items() returns each key:value pair as tuple
+        string += str(count) + " "
+    print(string)
+
+if __name__ == "__main__" :
+    l1 = open("./rosalind_dna.txt", "r")
+    l2 = l1.readline().strip() # remove all the white/tab space
+    counting(l2)
